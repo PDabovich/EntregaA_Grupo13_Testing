d645baf81a71005872bb0d999a6cbd0abec85e2f
Pablo <pdabovich@uc.cl>
2022-04-26 08:57:01 -0400

    draw board fix

--- view.rb
+++ view.rb
@@ -1,67 +1,56 @@
-require_relative 'naval_models'
+pieces = { 'side' => '│', 'topbot' => '─', 'tl' => '┌', 'tr' => '┐',
+           'bl' =>'└', 'br' => '┘', 'lc' => '├', 'rc' => '┤',
+           'tc' => '┬', 'bc' => '┴', 'crs' => '┼' }
 
-def drawgrid(args, boxlen = 3)
-    #Define box drawing characters
-    side = '│'
-    topbot = '─'
-    tl = '┌'
-    tr = '┐'
-    bl = '└'
-    br = '┘'
-    lc = '├'
-    rc = '┤'
-    tc = '┬'
-    bc = '┴'
-    crs = '┼'
-    ##############################
-    draw = []
-    args.each_with_index do |row, rowindex|
-      # TOP OF ROW Upper borders
-      row.each_with_index do |col, colindex|
-        if rowindex == 0
-          colindex == 0 ? start = tl : start = tc
-          draw << start + (topbot*boxlen)
-          colindex == row.length - 1 ? draw << tr : ""
-        end
-      end
-      draw << "\n" if rowindex == 0
-  
-      # MIDDLE OF ROW: DATA
-      row.each do |col|
-        draw << side + col.to_s.center(boxlen)
-      end
-      draw << side + "\n"
-  
-      # END OF ROW
-      row.each_with_index do |col, colindex|
-        if colindex == 0
-          rowindex == args.length - 1 ? draw << bl : draw << lc
-          draw << (topbot*boxlen)
-        else
-          rowindex == args.length - 1 ? draw << bc : draw << crs
-          draw << (topbot*boxlen)
-        end
-        endchar = rowindex == args.length - 1 ? br : rc
-  
-        #Overhang elimination if the next row is shorter
-        if args[rowindex+1]
-          if args[rowindex+1].length < args[rowindex].length
-            endchar = br
-          end
-        end
-        colindex == row.length - 1 ? draw << endchar : ""
-      end
-      draw << "\n"
-    end
-  
-  
-    draw.each do |char|
-      print char
-    end
-    return true
+def upper(boxlen, draw, pieces, row, rowindex)
+  row.each_with_index do |col, colindex|
+  if rowindex == 0
+    colindex == 0 ? start = pieces['tl'] : start = pieces['tc']
+    draw << start + (pieces['topbot']*boxlen)
+    colindex == row.length - 1 ? draw << pieces['tr'] : ""
   end
-  
-  drawgrid([[123,456,789,'baz'], ['abc','def','ghi'], ['foo', 'bar', '0'], ['buz', 'X', 'X']])
-  
-  
-       
+  end
+  draw << "\n" if rowindex == 0
+end
+
+def middle(boxlen, draw, pieces, row)
+  row.each do |col|
+  draw << pieces['side'] + col.to_s.center(boxlen)
+  end
+  draw << pieces['side'] + "\n"
+end
+
+def bottom(args, boxlen, draw, pieces, row, rowindex)
+  row.each_with_index do |col, colindex|
+  if colindex == 0
+    rowindex == args.length - 1 ? draw << pieces['bl'] : draw << pieces['lc']
+    draw << (pieces['topbot']*boxlen)
+  else
+    rowindex == args.length - 1 ? draw << pieces['bc'] : draw << pieces['crs']
+    draw << (pieces['topbot']*boxlen)
+  end
+  endchar = rowindex == args.length - 1 ? pieces['br'] : pieces['rc']
+  if args[rowindex+1]
+    if args[rowindex+1].length < args[rowindex].length
+      endchar = pieces['br']
+    end
+  end
+  colindex == row.length - 1 ? draw << endchar : ""
+  end
+  draw << "\n"
+end
+
+def drawgrid(args, boxlen = 3, pieces)
+  draw = []
+  args.each_with_index do |row, rowindex|
+    upper(boxlen, draw, pieces, row, rowindex)
+    middle(boxlen, draw, pieces, row)
+    bottom(args, boxlen, draw, pieces, row, rowindex)
+  end
+  draw.each do |char|
+    print char
+  end
+  return true
+end
+
+drawgrid([[123,456,789,'baz'], ['abc','def','ghi', 'X'], ['foo', 'bar', '0'], ['buz', 'X', 'X']], pieces)
